b6a65ee3dc848aacf0a4aab0f0430cbb0fc21a4f
YoungSeon.Ahn <ys.ahn@outlook.com>
2019-07-30 17:16:01 +0900

    Mock auth

---
+++ src/views/Auth.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-content>
+    <v-container
+      fluid
+      fill-height
+    >
+      <v-layout
+        align-center
+        justify-center
+      >
+        <v-flex
+          xs12
+          sm8
+          md4
+        >
+          <v-card class="elevation-12">
+            <v-toolbar
+              color="primary"
+              dark
+              flat
+            >
+              <v-toolbar-title>Login form</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="Login"
+                  name="login"
+                  prepend-icon="mdi-account"
+                  type="text"
+                ></v-text-field>
+
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="mdi-lock"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+	export default {
+		name: "Auth",
+	  props: {
+		  source: String,
+	  }
+	}
+</script>
+
+<style scoped>
+
+</style>
